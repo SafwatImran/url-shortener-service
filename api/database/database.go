d300f7cb978216955989b9f9b7f6211c98dd7884
Mohammed Safwat Imran <safwatimran@gmail.com>
2024-05-12 22:59:57 +0600

    add database client and resolve url functions

--- api/database/database.go
+++ api/database/database.go
@@ -1,1 +1,24 @@
 package database
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var Ctx = context.Background()
+
+var client *redis.Client
+
+func GetClient(dbNo int) *redis.Client {
+	if client == nil {
+		rdb := redis.NewClient(&redis.Options{
+			Addr:     os.Getenv("DB_ADDR"),
+			Password: os.Getenv("DB_PASS"),
+			DB:       dbNo,
+		})
+
+		client = rdb
+	}
+	return client
+}
--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,35 @@
 package routes
+
+import (
+	"net/http"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+	"github.com/safwatimran/url-shortener-service/api/database"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+
+	url := c.Params("url")
+
+	r := database.GetClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short url not found.",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "server error.",
+		})
+	}
+
+	rateIncrement := database.GetClient(1)
+	defer rateIncrement.Close()
+
+	_ = rateIncrement.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, http.StatusMovedPermanently)
+}
